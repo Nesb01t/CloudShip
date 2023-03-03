1eff70d5842b748e971e0f2ceb1951d8f37c2cd3
nesb01t <nesb01t@hotmail.com>
2023-03-03 19:18:04 +0800

    Update color consolelog

--- backend/main.js
+++ backend/main.js
@@ -1,4 +1,5 @@
 const koa = require("koa");
+const colors = require("colors");
 const hello = require("./middleware/hello");
 const responseTime = require("./middleware/response-time");
 const app = new koa();
@@ -7,6 +8,8 @@ app.use(hello);
 app.use(responseTime);
 
 runableLog = () => {
-  console.log("Server running at http://localhost:3000/");
+  console.log("------------------------------------------".yellow.bold);
+  console.log(" Server Running -> http://localhost:3000/");
+  console.log("------------------------------------------".yellow.bold);
 };
 app.listen(3000, runableLog);
