a869fec1d6490d61456a5a7ee430c1eb77aefb94
chenmh0901 <chenmh0901@foxmail.com>
2023-03-05 23:00:14 +0800

    Update index

--- frontend/pages/index/index.vue
+++ frontend/pages/index/index.vue
@@ -1,55 +1,55 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
+		<u-avatar :src="src" size="200rpx" shape="circle" style="margin: 0 100rpx;"></u-avatar>
 		<view class="text-area">
-			<text class="title">{{title}}</text>
+			<text class="title">{{ title }}</text>
 		</view>
 		<view class="contact">
-			<p>联系人</p><input type="text" placeholder="联系人">
+			<u--input type="number" placeholderStyle="#909399" placeholder="学号" border="surround" v-model="value1" @change="change"></u--input>
+			<u--input type="password" placeholderStyle="#909399" placeholder="密码" border="surround" v-model="value2" @change="change"></u--input>
 		</view>
+		<view class="btn"><u-button text="登录" size="large" color="linear-gradient(to right, rgb(66, 83, 216), rgb(213, 51, 186))"></u-button></view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello，叶俊豪'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
+export default {
+	data() {
+		return {
+			src: '/static/logo.png',
+			title: 'Hello，叶俊豪',
+			value1: '',
+			value2: ''
+		};
+	},
+	onLoad() {},
+	methods: {
+		change(e) {
+			console.log('change', e);
 		}
 	}
+};
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+.content {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	margin: 250rpx 0;
+}
+.btn {
+	margin: 50rpx 0;
+}
+.text-area {
+	display: flex;
+	justify-content: center;
+	margin: 40rpx 0;
+}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+.title {
+	font-size: 36rpx;
+	color: #8f8f94;
+}
 </style>
