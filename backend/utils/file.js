8bbb96f4f833111f5fd399e6b7499c08391063ea
nesb01t <littlelumos@foxmail.com>
2023-03-06 19:40:05 +0800

    Update 实现了登录和注册功能Restful API

--- backend/utils/file.js
+++ backend/utils/file.js
@@ -4,22 +4,27 @@ const path = require("path");
 defaultContext = {};
 
 module.exports = {
-  createFile(dirName, fileName, context) {
+  /**
+   * 在指定位置创建文件并写入内容
+   * 如果已创建则只写入
+   */
+  async writeFile(dirName, fileName, context) {
     // 初始化文件夹
-    if (!fs.existsSync("../data/" + dirName)) {
+    var existFolder = await fs.existsSync("../data/" + dirName);
+    if (!existFolder) {
       fs.mkdirSync("../data/" + dirName);
     }
 
-    // 写入内容
-    if (context == null) {
-      context == defaultContext;
-    }
     fs.writeFileSync(
       "../data/" + dirName + "/" + fileName + ".json",
       JSON.stringify(context)
     );
   },
-  readFile(dirName, fileName) {
+
+  /**
+   * 读取JSON中的对象
+   */
+  async readFile(dirName, fileName) {
     if (this.isFileExist(dirName, fileName)) {
       var context = fs.readFileSync(
         "../data/" + dirName + "/" + fileName + ".json"
@@ -27,7 +32,12 @@ module.exports = {
       return JSON.parse(context);
     }
   },
-  isFileExist(dirName, fileName) {
-    return fs.existsSync("../data/" + dirName + "/" + fileName + ".json");
+
+  /**
+   * 判断文件是否存在
+   */
+  async isFileExist(dirName, fileName) {
+    var bool = fs.existsSync("../data/" + dirName + "/" + fileName + ".json");
+    return bool;
   },
 };
